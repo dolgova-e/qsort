c3594a84a50398b5f8635d2b6a583cd82c79f2a1
Kate_Dolgova <katvolga@mail.ru>
2021-03-02 17:06:37 +0300

    multy

--- sortTwoMethods.py
+++ sortTwoMethods.py
@@ -1,12 +1,13 @@
 from random import randint
+from multiprocessing import Process
 import random
 import timeit
 
 
 # сортировка "пузырьком"
 def bubbleSort(massive):
-    for i in range(N - 1):
-        for i in range(N - 1):
+    for i in range(len(massive) - 1):
+        for i in range(len(massive) - 1):
             if massive[i] <= massive[i + 1]:
                 maxElem = massive[i + 1]
             else:
@@ -17,29 +18,36 @@ def bubbleSort(massive):
 
 
 # сортировка Хоара
-def hoarSort(nums):
-    if len(nums) <= 1:
-        return nums
+def hoarSort(massive):
+    if len(massive) <= 1:
+        return massive
     else:
-        q = random.choice(nums)
-        s_nums = []
-        m_nums = []
-        e_nums = []
-        for n in nums:
+        q = random.choice(massive)
+        s_massive = []
+        m_massive = []
+        e_massive = []
+        for n in massive:
             if n < q:
-                s_nums.append(n)
+                s_massive.append(n)
             elif n > q:
-                m_nums.append(n)
+                m_massive.append(n)
             else:
-                e_nums.append(n)
-        return hoarSort(s_nums) + e_nums + hoarSort(m_nums)
+                e_massive.append(n)
+        return hoarSort(s_massive) + e_massive + hoarSort(m_massive)
 
 
-print('Введите количество элементов: ', sep='', end='')
-N = int(input())
-sortElements = [randint(-1000, 1000) for i in range(N)]
-print(*sortElements, '- исходный список')
-print(*bubbleSort([sortElements[i] for i in range(N)]), '- список отсортирован "Пузырьком" за',
-      timeit.timeit("bubbleSort(sortElements)", globals=globals()), 'секунд')
-print(*hoarSort([sortElements[i] for i in range(N)]), '- список отсортирован сортировкой Хоара за',
-      timeit.timeit("hoarSort(sortElements)", globals=globals()), 'секунд')
+if __name__ == '__main__':
+    print('Введите количество элементов: ', sep='', end='')
+    N = int(input())
+    sortElements = [randint(-1000, 1000) for i in range(N)]
+    process_1 = Process(target=bubbleSort, args=('massive',))
+    process_2 = Process(target=hoarSort, args=('massive',))
+    process_1.start()
+    process_2.start()
+    process_1.join()
+    process_2.join()
+    print(*sortElements, '- исходный список')
+    print(*bubbleSort([sortElements[i] for i in range(N)]), '- список отсортирован "Пузырьком" за',
+          timeit.timeit("bubbleSort(sortElements)", globals=globals()), 'секунд')
+    print(*hoarSort([sortElements[i] for i in range(N)]), '- список отсортирован сортировкой Хоара за',
+          timeit.timeit("hoarSort(sortElements)", globals=globals()), 'секунд')
